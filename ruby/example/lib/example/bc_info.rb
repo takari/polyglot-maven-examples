7e838506ed4a0cc1171598421706e3d4c6bd987e
Manfred Moser <manfred@simpligility.com>
2017-05-23 11:51:03 -0700

    removed generated files

--- ruby/example/lib/example/bc_info.rb
+++
@@ -1,9 +0,0 @@
-java_import org.bouncycastle.jce.provider.BouncyCastleProvider
-
-module Example
-
-  def self.bc_info
-    BouncyCastleProvider.new.info
-  end
-
-end
--- ruby/example/lib/example_jars.rb
+++
@@ -1,5 +0,0 @@
-# this is a generated file, to avoid over-writing delete this comment
-require 'jar_dependencies'
-
-require_jar( 'org.bouncycastle', 'bcpkix-jdk15on', '1.55' )
-require_jar( 'org.bouncycastle', 'bcprov-jdk15on', '1.55' )
