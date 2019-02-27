1a51ce093179a9020da9607a3750e2bed6e10ed4
Robert Thornton <ThorntonRP@ldschurch.org>
2019-02-27 21:01:28 +0000

    updates for polyglot-maven PR #182

--- runAll.sh
+++ runAll.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -e
 
+additionalArgs=$@
+
 polytest () {
   printf "=====================================================\n\n"
   printf "          Testing polyglot-$1 started. \n\n"
   printf "=====================================================\n\n"
   cd $1
-  mvn clean install -U
+  mvn $additionalArgs clean install -U
   printf "=====================================================\n\n"
   printf "          Testing polyglot-$1 completed. \n\n"
   printf "=====================================================\n\n"
